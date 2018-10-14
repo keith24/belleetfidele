a5287e3c8090ce1c3cc9da6f8e79ba3db3102bea
Keith Irwin <mail@keithirwin.us>
2018-10-14 14:49:46 -0600

    Removed skeleton, added stripe checkout

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,24 @@ function countWords(s) {
   if (word_array.length===1&&word_array[0]==="") return 0
   else return word_array.length
 }
+
+const buyHandler = StripeCheckout.configure({
+  key: 'pk_test_absAcyl9d70mNZaoLomaOczZ',
+  locale: 'auto',
+  token: function(token) {
+  	console.log(token.id)
+    // TODO: Send token ID, and text to my email
+  }
+})
+document.getElementById('buy-btn').addEventListener('click', function(e) {
+  buyHandler.open({
+    name: 'Translation',
+    description: 'Translation of '+document.getElementById('wordcount').innerHTML+' words into English',
+    email: document.getElementById('buyer-email').value,
+    amount: parseFloat(document.getElementById('cost').innerHTML)*100,
+  })
+  e.preventDefault()
+})
+window.addEventListener('popstate', function() {
+  buyHandler.close()
+});
