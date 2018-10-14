4fe459949e784f4869e5a3723fdd74f588f79a37
Keith Irwin <mail@keithirwin.us>
2018-10-13 19:11:01 -0600

    Fixed word counter and price calculator

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
 ko.applyBindings(function() {
   this.submission = ko.observable('')
-  this.wordcount = ko.computed(function() {
-  	return this.submission().split(' ').length
-	})
+  this.wordcount = ko.computed(function () {
+    return countWords(this.submission())
+  })
 	this.price = ko.computed(function() {
-	  return parseInt(this.wordcount(), 10) * 3
+	  return (parseInt(this.wordcount(), 10) * .3).toFixed(2)
+	})
+	this.pluralWords = ko.computed(function() {
+	  return this.wordcount()!==1
 	})
 })
+
+function countWords(s) {
+  const word_array = s.replace(/^\s+|\s+$/g,"").split(/\s+/)
+  if (word_array.length===1&&word_array[0]==="") return 0
+  else return word_array.length
+}
