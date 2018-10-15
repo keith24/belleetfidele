b79ee0ecd2c2e09a5019afa1a375ae73cb608dcd
Keith Irwin <mail@keithirwin.us>
2018-10-14 18:48:45 -0600

    Added more form logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+// Global vars
+const xhr = new XMLHttpRequest()
+
 ko.applyBindings(function() {
   this.submission = ko.observable('')
   this.wordcount = ko.computed(function () {
@@ -21,19 +24,61 @@ const buyHandler = StripeCheckout.configure({
   key: 'pk_test_absAcyl9d70mNZaoLomaOczZ',
   locale: 'auto',
   token: function(token) {
-  	console.log(token.id)
-    // TODO: Send token ID, and text to my email
+  
+    // POST the data!
+    xhr.open('POST', 'https://formspree.io/translate@ki9.us')
+    xhr.send({
+      'token': token.id,
+      'email': document.getElementById('email').value,
+      'submission': document.getElementById('submission').value,
+      'wordcount': document.getElementById('wordcount').innerHTML,
+      'cost': document.getElementById('cost').innerHTML,
+    })
+    
+    // And clear that form! 
+	  document.getElementById('email').value = ''
+	  document.getElementById('submission').value = ''
+	  document.getElementById('wordcount').innerHTML = '0'
+	  
   }
 })
+
+// Form submission
 document.getElementById('buy-btn').addEventListener('click', function(e) {
-  buyHandler.open({
-    name: 'Translation',
-    description: 'Translation of '+document.getElementById('wordcount').innerHTML+' words into English',
-    email: document.getElementById('buyer-email').value,
-    amount: parseFloat(document.getElementById('cost').innerHTML)*100,
-  })
-  e.preventDefault()
+  
+  // TODO: Make the red boxes normal again
+  
+  email = document.getElementById('email').value
+  wordcount = document.getElementById('wordcount').innerHTML
+  cost = document.getElementById('cost').innerHTML
+  
+  // Validations
+  if (wordcount==0) {
+    alert('The translation of nothing is still nothing...')
+    //TODO: Make submission box red or whatever
+  } else if (email==='') {
+    alert('Please enter an email address')
+    //TODO: Make email box red or whatever
+  } else if (!validEmail(email)) {
+    alert('Somethings funny about that email address...')
+    //TODO: Make email box red or whatever
+  }
+  
+  // All valid, open purchase popup
+  else buyHandler.open({
+	  name: 'Translation',
+	  description: 'Translation of '+wordcount+' words into English',
+	  email: email,
+	  amount: parseFloat(cost)*100,
+	})
+  
 })
+// Stripe wants this to be included
 window.addEventListener('popstate', function() {
   buyHandler.close()
-});
+})
+
+// https://stackoverflow.com/a/46181/3006854
+function validEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(email).toLowerCase());
+}
